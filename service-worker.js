f19e5027f905e71d90ab4bde12138b7ff8589a4e
SaraLopes7 <sariinha.lopes2003@gmail.com>
2023-04-11 13:51:43 -0300

    arrumando service_worker :mask:

--- service-worker.js
+++ service-worker.js
@@ -19,6 +19,25 @@
             'fonts.googleapis.com',
             'cdn.jsdelivr.net'
         ]
+
+        function cleanResponse(response) {
+            const clonedResponse = response.clone();
+          
+            // Not all browsers support the Response.body stream, so fall back to reading
+            // the entire body into memory as a blob.
+            const bodyPromise = 'body' in clonedResponse ?
+              Promise.resolve(clonedResponse.body) :
+              clonedResponse.blob();
+          
+            return bodyPromise.then((body) => {
+              // new Response() is happy when passed either a stream or a Blob.
+              return new Response(body, {
+                headers: clonedResponse.headers,
+                status: clonedResponse.status,
+                statusText: clonedResponse.statusText,
+              });
+            });
+          }
     
         // The Util Function to hack URLs of intercepted requests
         const getFixedUrl = (req) => {
