71da76bcaffb621548553c466fa1a19eebd69083
Filipe Lacerda <gdfkaiser@gmail.com>
2023-04-13 15:12:28 -0300

    6.0

---
+++ rotas.js
@@ -0,0 +1,43 @@
+function abreCapacete() {
+  window.open('https://epi-assure.netlify.app/usuario/estudos-det', '_self');
+}
+
+function abreBota() {
+  window.open('https://epi-assure.netlify.app/usuario/estudos-det', '_self');
+}
+
+function abreColete() {
+  window.open('https://epi-assure.netlify.app/usuario/estudos-det', '_self');
+}
+
+function abreLuva() {
+  window.open('https://epi-assure.netlify.app/usuario/estudos-det', '_self');
+}
+
+function abreFita() {
+  window.open('https://epi-assure.netlify.app/usuario/estudos-det', '_self');
+}
+
+function abreOculos() {
+  window.open('https://epi-assure.netlify.app/usuario/estudos-det', '_self');
+}
+
+function abreMascara() {
+  window.open('https://epi-assure.netlify.app/usuario/estudos-det', '_self');
+}
+
+function abreCone() {
+  window.open('https://epi-assure.netlify.app/usuario/estudos-det', '_self');
+}
+
+function abreSolicitar() {
+   window.open('https://epi-assure.netlify.app/usuario/solicitar', '_self');
+}
+
+function sair() {
+  window.open('https://epi-assure.netlify.app/login', '_self');
+}
+
+function volta() {
+  window.history.back();
+}
