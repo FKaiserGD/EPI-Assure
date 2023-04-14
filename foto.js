ac16d6688d10334342df87e82653ec1324ef78c0
Filipe Lacerda <gdfkaiser@gmail.com>
2023-04-14 17:46:27 -0300

    7.1.2

--- foto.js
+++ foto.js
@@ -1,14 +1,14 @@
 function tiraFoto() {
-    const takePictureBtn = document.getElementById('take-picture-btn');
-    const picture = document.getElementById('picture');
+  const takePictureBtn = document.getElementById('take-picture-btn');
+  const picture = document.getElementById('picture');
 
-    takePictureBtn.addEventListener('click', function() {
-        const canvas = document.createElement('canvas');
-        canvas.width = cameraStream.videoWidth;
-        canvas.height = cameraStream.videoHeight;
-        canvas.getContext('2d').drawImage(cameraStream, 0, 0, canvas.width, canvas.height);
-        const dataUrl = canvas.toDataURL('image/png');
-        picture.setAttribute('src', dataUrl);
-        picture.style.display = "block";
-    });
+  takePictureBtn.addEventListener('click', function() {
+    const canvas = document.createElement('canvas');
+    canvas.width = cameraStream.videoWidth;
+    canvas.height = cameraStream.videoHeight;
+    canvas.getContext('2d').drawImage(cameraStream, 0, 0, canvas.width, canvas.height);
+    const dataUrl = canvas.toDataURL('image/png');
+    picture.removeAttribute('style');
+    picture.setAttribute('src', dataUrl);
+  });
 }
