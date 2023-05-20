6eac5ada8b57c5e66a83fb9283ad8eb8a3082a89
Glysonn <kauaglyson@gmail.com>
2023-05-20 13:28:20 -0300

    feat: Criando a classe de Checkin

---
+++ backend/models/Checkin.cs
@@ -0,0 +1,21 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace backend.models
+{
+    public class Checkin
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)] 
+        public string _id { get; set; }
+
+        [BsonElement("codigoUsuario")]
+        public string CodigoUsuario { get; set; }
+        [BsonElement("data")]
+        public DateTime Data { get; set; }
+        [BsonElement("linkImagemComprobatoria")]
+        public string LinkImagemComprobatoria { get; set; }
+        [BsonElement("equipamentos")]
+        public List<Equipamento> Equipamentos { get; set; }
+    }
+}
