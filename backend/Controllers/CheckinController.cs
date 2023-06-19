46ebbd4431bd368794766616a93b5c1fcf36d8f3
Glysonn <kauaglyson@gmail.com>
2023-06-19 02:09:58 -0300

    implementando o update e delete da controller checkin

--- backend/Controllers/CheckinController.cs
+++ backend/Controllers/CheckinController.cs
@@ -45,5 +45,46 @@ namespace backend.Controllers
                 return StatusCode(500, ex.Message);
             }
         }
+
+        [HttpPut]
+        [Route("UpdateCheckin/{id}")]
+        public IActionResult UpdateCheckin(string id, Checkin updatedCheckin)
+        {
+            try
+            {
+                var filter = Builders<Checkin>.Filter.Eq("_id", id);
+                var result = _checkinCollection.ReplaceOne(filter, updatedCheckin);
+
+                if (result.ModifiedCount == 1)
+                    return Ok(updatedCheckin);
+                else
+                    return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("DeleteCheckin/{id}")]
+        public IActionResult DeleteCheckin(string id)
+        {
+            try
+            {
+                var filter = Builders<Checkin>.Filter.Eq("_id", id);
+                var result = _checkinCollection.DeleteOne(filter);
+
+                if (result.DeletedCount == 1)
+                    return Ok();
+                else
+                    return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
     }
 }
